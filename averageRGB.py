95a7a341ad1be2ea80e1c56e5e8ec0ac98ac7edd
noam wolf <noamswolf@gmail.com>
2024-07-11 12:27:50 +0300

    finished script

--- averageRGB.py
+++ averageRGB.py
@@ -1,24 +1,12 @@
 # importing required libraries 
 import cv2 
-import numpy as np
-import time
+import numpy as np	
 import matplotlib.pyplot as plt
 
-# taking the input from webcam 
-vid = cv2.VideoCapture(0) 
 
-# capturing the current frame 
-_, frame = vid.read()
-# displaying the current frame 
-cv2.imshow("frame", frame)
-cv2.waitKey(500)
 
-blue, green, red = cv2.split(frame)
 
-# running while loop just to make sure that 
-# our program keep running until we stop it 
-while True: 
-
+def captureImage(vid):
 	# capturing the current frame 
 	_, frame = vid.read() 
 
@@ -26,23 +14,36 @@ while True:
 	cv2.imshow("frame", frame)
 	
 	cv2.waitKey(1000)
+	return frame
 
-	# setting values for base colors 
-	b = frame[:, :, 2] 
-	g = frame[:, :, 1] 
-	r = frame[:, :, 0] 
+def returnColorWithHighestMean(frame):
+	blue_channel, green_channel, red_channel = cv2.split(frame)
 
 	# computing the mean 
-	b_mean = np.mean(b) 
-	g_mean = np.mean(g) 
-	r_mean = np.mean(r) 
+	b_mean = np.mean(blue_channel) 
+	g_mean = np.mean(green_channel) 
+	r_mean = np.mean(red_channel) 
 	print(f'r mean: {r_mean}')
 	print(f'g mean: {g_mean}')
 	print(f'b mean: {b_mean}')
 	# displaying the most prominent color 
 	if (b_mean > g_mean and b_mean > r_mean): 
-		print("Blue") 
+		return("Blue") 
 	elif (g_mean > r_mean and g_mean > b_mean): 
-		print("Green") 
+		return("Green") 
 	else: 
-		print("Red")
+		return("Red")
+
+def main():
+	# taking the input from webcam 
+	vid = cv2.VideoCapture(0) 
+	# running while loop just to make sure that 
+	# our program keep running until we stop it 
+	while True: 
+
+		frame = captureImage(vid)
+		color = returnColorWithHighestMean(frame)
+		print(color)
+
+if __name__ == '__main__':
+	main()
